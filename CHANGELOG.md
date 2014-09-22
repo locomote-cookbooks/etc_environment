efb7a9c8b18f6f5fc7ff85e47cae678de171706e
chris <chris@locomote.com.au>
2014-09-22 12:33:37 +1000

    Use JSON for env var values
    (ie. so we can serialize/de-serialize more complex structures via JSON)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.1.0 / 2014-09-22
+Uses JSON for env var values
+(ie. so we can serialize/de-serialize more complex structures via JSON)
+
 ## 1.0.1 / 2013-07-03
 First public release with test-kitchen.
 
--- test/integration/default/shpec/etc_environment_shpec.sh
+++ test/integration/default/shpec/etc_environment_shpec.sh
@@ -1,7 +1,8 @@
 describe "etc_environment"
   it "should create /etc/environment entries"
     grep 'DB_HOST="localhost"' /etc/environment &&
-    grep 'DB_PORT=3306' /etc/environment
+    grep 'DB_PORT=3306' /etc/environment &&
+    grep 'SOME_JSON="{\"a\":1,\"b\":\"2\",\"c\":{\"d\":\"three\"}}"' /etc/environment
     assert equal "$?" 0
 
   it "should set environment variable globally"
