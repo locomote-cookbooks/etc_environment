81fd07ce02202c0acadc63d709fe8568f225d7d2
chris <chris@locomote.com.au>
2015-01-21 23:04:34 +1100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Chris"
 maintainer_email "chris@locomote.com"
 license          "BSD"
 description      "Populates /etc/environment"
-version          "1.1.1"
+version          "1.2.0"
 
 %w{redhat centos}.each do |os|
   supports os
